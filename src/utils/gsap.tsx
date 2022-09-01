d9a1cb157369f59ae26a629ff580d04218878bc0
Kutay Sakallioglu <kutay@vestiyer.co>
2022-09-01 20:31:25 +0300

    test7

--- src/utils/gsap.tsx
+++ src/utils/gsap.tsx
@@ -1,7 +1,7 @@
 // SSR doesn't work with ES Modules so we need to import the UMD files instead
 
 
-import { gsap } from "gsap";
+import { gsap } from 'gsap/dist/gsap'
 import { Draggable } from "gsap/dist/Draggable";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
